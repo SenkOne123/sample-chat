3e57e38f1e6ae1b7c5adcdc7eda9aa6270519334
Nikita Mikryukov <senkone0123@gmail.com>
2023-03-19 22:49:21 +0500

    feat(): Try to debug request and headers;

--- src/app/authentication.interceptor.ts
+++ src/app/authentication.interceptor.ts
@@ -17,6 +17,7 @@ export class AuthenticationInterceptor implements HttpInterceptor {
     public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
         const clonedRequest = request.clone();
         clonedRequest.headers.set('Content-type', 'application/json');
+        console.log(request);
 
         const token = this.authService.token$.value;
         if (token) {
